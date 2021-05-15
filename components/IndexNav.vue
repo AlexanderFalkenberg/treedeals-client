bef3f64b8a366f67e595d0bd8b240367b4503ab7
AlexanderFalkenberg <35661687+AlexanderFalkenberg@users.noreply.github.com>
2021-05-15 16:00:56 +0200

    index

--- components/IndexNav.vue
+++ components/IndexNav.vue
@@ -4,9 +4,9 @@
       <div
         class="max-w-7xl text-xs font-bold mx-auto py-2 flex item-center justify-between px-2 sm:px-4 lg:px-10 overflow-x-auto whitespace-nowrap space-x-4 text-center"
       >
+        <p>Schnäppchen, Angebote und Gutscheine</p>
         <p>
-          Für jeden erfolgreich vermittelten Deal leisten wir einen Beitrag zum
-          Umweltschutz
+          Ein Großteil unseres Gewinns investieren wir in Baumbestände weltweit
         </p>
         <p>Große Zahl an preiswerten und nachhaltigen Produkten</p>
       </div>
